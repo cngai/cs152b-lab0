536dd5c478ee0cf8206d9a9cebf029cbaaf02636
cngai <cngai1223@gmail.com>
2020-04-01 21:29:09 -0700

    part 2: make diff either equal to 0 or 11

--- mult11.v
+++ mult11.v
@@ -11,13 +11,15 @@ module mult_11(
 
   reg [4:0] odd_sum;
   reg [4:0] even_sum;
+  reg [4:0] diff;
 
   initial
     begin
       assign odd_sum = in[15:12] + in[7:4];
       assign even_sum = in[11:8] + in[3:0];
+      assign diff = odd_sum - even_sum;
 
-      assign out = (odd_sum - even_sum) === 0;
+      assign out = diff === 0 || diff === 5'b01011;
     end
 
 endmodule
